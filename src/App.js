2bcc62feb9dbfe76741ad45df0df25dc2881e4e7
ShobhitSagar <shobhitsagar109@gmail.com>
2020-10-30 06:11:44 +0530

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Game from './components/Game'
-import Score from './components/Score'
 import React, { useRef, useState } from 'react';
 
 function App() {
@@ -8,14 +7,11 @@ function App() {
   const [userHand, setUserHand] = useState(0);
   const [botHand, setBotHand] = useState(0);
 
-  const [userScore, setUserScore] = useState(0);
-  const [botScore, setBotScore] = useState(0);
-
   const [animFlag, setAnimFlag] = useState(false);
   const game_screen = useRef();
 
   const genUserHand = (e) => {
-	  setAnimFlag(!animFlag);
+	  setAnimFlag(true);
     setUserHand(parseInt(e.target.value));
     genBotHand();
   }
@@ -23,17 +19,6 @@ function App() {
   const genBotHand = () => {
     const botNum = Math.floor(Math.random() * 3) + 1;
     setBotHand(botNum);
-    setScore();
-  }
-
-  const setScore = () => {
-    if (userHand !== botHand) {
-        if ((userHand === 1 && botHand === 3) || (userHand === 2 && botHand === 1) || (userHand === 3 && botHand === 2)) {
-            setUserScore(userScore + 1);
-        } else {
-            setBotScore(botScore + 1);
-        }
-    }
   }
 
   const start = (e) => {
@@ -41,22 +26,19 @@ function App() {
     setTimeout(() => {
       e.target.style.display = "none";
       game_screen.current.classList.add('animate__zoomIn');
-      game_screen.current.style.display = "flex";
+      game_screen.current.style.display = 'flex';
     }, 1000);
-    setUserHand();
   }
 
   return (
     <div className="App">
-        <button onClick={start} className="start animate__animated">Start</button>
+      <button onClick={start} className="start animate__animated">Start</button>
       <div ref={game_screen} className="main_screen animate__animated">
-        <Score userScore={userScore} botScore={botScore}/>
-        <Game animFlag={animFlag}/>
-
-        <div>
-            <button onClick={genUserHand} value="1">Rock</button>
-            <button onClick={genUserHand} value="2">Paper</button>
-            <button onClick={genUserHand} value="3">Scissor</button>
+        <Game animFlag={animFlag} setAnimFlag={setAnimFlag} userHand={userHand} botHand={botHand}/>
+        <div className="buttons">
+          <button id="button" onClick={genUserHand} value="1" className="far fa-hand-rock"></button>
+          <button id="button" onClick={genUserHand} value="2" className="far fa-hand-paper"></button>
+          <button id="button" onClick={genUserHand} value="3" className="far fa-hand-peace"></button>
         </div>
       </div>
     </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,61 @@
 import './Game.css'
-import React, { useEffect, useState } from 'react';
+import Score from './Score'
+import React, { useEffect, useRef, useState } from 'react';
 
-function Game({animFlag}) {
+function Game({animFlag, setAnimFlag, userHand, botHand}) {
 
-    const [userClass, setUserClass] = useState('fas fa-hand-paper');
-    const [botClass, setBotClass] = useState('fas fa-hand-paper');
+    const [userClass, setUserClass] = useState('fa-hand-paper');
+    const [botClass, setBotClass] = useState('fa-hand-paper');
+
+    const [userScore, setUserScore] = useState(0);
+    const [botScore, setBotScore] = useState(0);
+
+    const user_hand = useRef();
 
     useEffect(() => {
-        setUserClass('fas fa-hand-rock');
-        setBotClass('fas fa-hand-rock');
-    });
+
+        const setUserHand = () => {
+            if (userHand === 1) {setUserClass('fa-hand-rock')}
+            else if (userHand === 2) {setUserClass('fa-hand-paper')}
+            else if (userHand === 3) {setUserClass('fa-hand-peace')}
+        }
+
+        const setBotHand = () => {
+            if (botHand === 1) {setBotClass('fa-hand-rock')}
+            else if (botHand === 2) {setBotClass('fa-hand-paper')}
+            else if (botHand === 3) {setBotClass('fa-hand-peace')}
+        }
+
+        if(animFlag) {
+            setAnimFlag(false);
+            if (userHand !== botHand) {
+                if ((userHand === 1 && botHand === 3) || (userHand === 2 && botHand === 1) || (userHand === 3 && botHand === 2)) {
+                    setUserScore(userScore + 1);
+                } else {
+                    setBotScore(botScore + 1);
+                }
+            }
+        // console.log("user TYPE : "+ typeof(userHand));
+        // if (animFlag > 0) {
+        //     setUserClass('fa-hand-rock');
+        //     user_hand.current.classList.add('user_hand_anim');
+        //     setTimeout(() => {
+                // user_hand.current.classList.remove('user_hand_anim');
+                setUserHand();
+                setBotHand();
+        }
+                // setUserClass('fa-hand-peace');
+        //     }, 1500);
+        // }
+        // setBotClass('fa-hand-rock');
+    }, [animFlag, setAnimFlag, userHand, botHand, userScore, botScore]);
 
     return(
-        <div>
-            <div className="flex_row">
-                <div id="user_hand"><i id="user_hand" className={userClass}></i></div>
-                <div id="bot_hand"><i className={botClass}></i></div>
+        <div className="game_col">
+            <Score userScore={userScore} botScore={botScore}/>
+            <div className="game_flex_row">
+                <div id="user_hand"><i ref={user_hand} id="user_hand" className={`fas ${userClass}`}></i></div>
+                <div id="bot_hand"><i className={`fas ${botClass}`}></i></div>
             </div>
         </div>
     )
